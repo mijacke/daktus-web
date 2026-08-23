3c9cbe11e44e13b353a03dbd989ed90222b613ac
mijacke <lassumario2002@gmail.com>
2026-08-24 00:22:04 +0200

    Add 96px favicon for Google Search

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,7 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
